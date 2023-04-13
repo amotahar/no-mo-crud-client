3b253c9a7b1ae2a617942da73f1916eb7ed25878
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-04-13 16:34:43 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  createBrowserRouter,RouterProvider
+} from "react-router-dom";
+import Home from './compnents/Home';
+import AddUser from './compnents/AddUser';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home></Home>
+
+    },
+    {
+      path: '/users/add',
+      element: <AddUser></AddUser>
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}>
+
+      </RouterProvider>
     </div>
   );
 }

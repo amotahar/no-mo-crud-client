aeb79a89dbf4cd5b16443b329d84254ecfb4b25d
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-04-14 23:23:06 +0600

    form

--- src/components/AddUsers.js
+++ src/components/AddUsers.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddUsers = () => {
+  const [user, setUser] =useState({});
+  const handleAddUser = event => {
+          event.preventDefault();
+  }
   return (
     <div>
-      <h2>This is Add Users</h2>
+      <h2>Please add a User</h2>
+        <form action="" onSubmit={handleAddUser}>
+              <input type="name" name="name" id="" placeholder="name" /> <br />
+              <input type="email" name="email" id="" placeholder="email" /> <br />
+              <button type='submit'>Add User</button>
+        </form>
     </div>
   );
 };

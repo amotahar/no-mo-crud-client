d765f2d2a2e786f163ef68fa6ff7fdc8cfb98f47
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-04-15 18:12:03 +0600

    adduser

--- src/components/AddUsers.js
+++ src/components/AddUsers.js
@@ -1,44 +1,49 @@
 import React, { useState } from 'react';
 
 const AddUsers = () => {
-  const [user, setUser] =useState({});
-  const handleAddUser = event => {
-          event.preventDefault();
-          console.log(user);
+  const [user, setUser] = useState({});
 
-          fetch('http://localhost:5000/users', {
-             method: 'POST',
-             headers: {
-              'content-type': 'application/json',
+    const handleAddUser = event =>{
+        event.preventDefault();
+        console.log(user);
+
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
             },
             body: JSON.stringify(user)
-          })
-          .then(res=>res.json())
-          .then(data => {
+        })
+        .then(res => res.json())
+        .then(data => {
             if(data.acknowledged){
-              alert('User added successfully')
-              event.target.reset();
+                alert('User added successfully');
+                event.target.reset();
             }
-          });
-  }
-  const handleInputBlur = event => {
-  const value = event.target.value;
-  const field = event.target.name;
-  const newUser = {...user};
-  newUser[field] = value;
-  setUser(newUser);
-     
-  }
-  return (
-    <div>
-      <h2>Please add a User</h2>
-        <form action="" onSubmit={handleAddUser}>
-              <input onBlur={handleInputBlur} type="name" name="name" id="" placeholder="name" required/> <br />
-              <input  onBlur={handleInputBlur} type="name" name="address" id="" placeholder="address" required/> <br />
-              <input  onBlur={handleInputBlur} type="email" name="email" id="" placeholder="email" required/> <br />
-              <button type='submit'>Add User</button>
-        </form>
-    </div>
+        })
+    }
+
+    const handleInputBlur = event =>{
+        const field = event.target.name;
+        const value = event.target.value;
+        const newUser = {...user}
+        newUser[field] = value;
+        setUser(newUser);
+    }
+
+    return (
+        <div>
+            <h2>Please add a new User</h2>
+            <form onSubmit={handleAddUser}>
+                <input onBlur={handleInputBlur} type="text" name='name' placeholder='name' required />
+                <br />
+                <input onBlur={handleInputBlur} type="text" name='address' placeholder='address' required />
+                <br />
+                <input onBlur={handleInputBlur} type="email" name="email" id="" placeholder='email' required />
+                <br />
+                <button type="submit">Add User</button>
+            </form>
+        </div>
   );
 };
 
